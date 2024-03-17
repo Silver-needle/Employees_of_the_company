a0a6da53d14e57444616669ff91de6440527ac0f
Margarita Samoray <msamoray161@gmail.com>
2024-03-17 21:30:39 +0300

    add_props

--- employees/src/components/employees-list/employees-list.js
+++ employees/src/components/employees-list/employees-list.js
@@ -5,9 +5,9 @@ import './employees-list.css';
 const EmployeesList = () => {
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            <EmployeesListItem name="John C." salary={800}  />
+            <EmployeesListItem name="Martin D." salary={3000}  />
+            <EmployeesListItem name="Carl U." salary={5000}  />
         </ul>
     )
 }
