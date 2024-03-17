a6c6b9b4c05519e4d1213f9de7a267041d8b7e30
Margarita Samoray <msamoray161@gmail.com>
2024-03-17 23:31:50 +0300

    0.1

--- employees/src/components/app/app.js
+++ employees/src/components/app/app.js
@@ -9,9 +9,9 @@ import './app.css';
 function App() {
 
     const data = [
-        {name: 'John C.', salary: 800, increase: false},
-        {name: 'Martin D.', salary: 3000, increase: true},
-        {name: 'Carl U.', salary: 5000, increase: false},
+        {name: 'John C.', salary: 800, increase: false, id: 1},
+        {name: 'Martin D.', salary: 3000, increase: true, id: 2},
+        {name: 'Carl U.', salary: 5000, increase: false, id: 3},
     ];
 
     return (
