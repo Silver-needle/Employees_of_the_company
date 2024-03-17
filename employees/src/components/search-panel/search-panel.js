c813846615ed85d153cdee9c7c09f5b7d26f53ca
Margarita Samoray <msamoray161@gmail.com>
2024-03-17 18:11:48 +0300

    add-components

---
+++ employees/src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input
+            type="text"
+            className="form-control search-iput"
+            placeholder="Найти сотрудника"/>
+    )
+}
+
+export default SearchPanel;
